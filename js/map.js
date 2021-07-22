c4403eab933f6d77773c67ab1705eb320659fc38
Jürij Pigowski <yuriy.pigovsky@qvantel.com>
2021-07-22 09:45:15 +0300

    Add Iosafata church timetable

--- js/map.js
+++ js/map.js
@@ -1,6 +1,48 @@
+const Churches = [
+    {
+        "lat": 50.46139554755741, 
+        "lng": 30.382982790470127,
+        "name": `
+<h1>УГКЦ Святого Йосафата</h1>
+
+<h2>Розклад Богослужень</h2>
+
+<h3>Понеділок-четвер</h3>
+<ul>
+    <li>8:00 Божественна літургія за здоров'я</li>
+</ul>
+
+<h3>П'ятниця</h3> 
+<ul>
+    <li>8:00 Божественна літургія за здоров'я</li>
+    <li>18:30 Молитви на оздоровлення</li>
+</ul>
+
+<h3>Субота</h3>
+<ul>
+    <li>9:00 Божественна літургія за упокій</li>
+    <li>10:00 Панахида</li>
+    <li>10:30 1/3 - Lectio Divina, 2/4 - Матері в молитві</li>
+    <li>18:00 Всеношня (Вечірня та Утреня)</li>
+</ul>
+
+<h3>Неділя</h3>
+<ul>
+    <li>9:00 Божественна літургія за здоров'я</li>
+    <li>11:00 Божественна літургія для дітей</li>
+</ul>
+`
+    }
+];
+
+
 function init() {
     // initialize Leaflet
-    var map = L.map('map').setView({lon: 0, lat: 0}, 2);
+    var map = L.map('map').setView([50.4614919,30.3828357], 20);
+
+    map.on('click', (event) => {
+        console.log(event.latlng);
+    });
 
     // add the OpenStreetMap tiles
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,6 +54,8 @@ function init() {
     L.control.scale().addTo(map);
 
     // show a marker on the map
-    L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(map);
+    for(church of Churches) {
+        L.marker([church.lat, church.lng]).bindPopup(church.name).addTo(map);
+    }
 }
 
