b988f533607c8a75edc53ac94e8a11e72f20a292
Yuriy Pigovsky <pigovsky@gmail.com>
2021-08-16 10:19:31 +0300

    Добав посилання на церкви Теребовлі

--- geo-data/objects.js
+++ geo-data/objects.js
@@ -1,5 +1,6 @@
 OBJECTS = {
     "Церкви Києва": "churches/Kyiv.html",
     "Церкви Тернополя": "churches/Ternopil.html",
+    "Церкви Теребовлі": "churches/Terebovlya.html",
     "Парки Києва": "parks/Kyiv.html"
 }
